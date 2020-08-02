12ce87e47fee24a39e63590b78ce62470fc77fa7
WenChiu <wen2214@gmail.com>
2020-08-02 21:03:10 +0800

    static layout

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, TextInput, View, FlatList } from 'react-native';
 
 export default function App() {
+  const [task, setTask] = useState("");
+  const arr = [
+    {taskNum: 1, task: "task1"}, 
+    {taskNum: 2, task: "task2"}, 
+    {taskNum: 3, task: "task3"}, 
+  ];
+
+  const renderItem = ({item}) => (
+    <View style={styles.item}>
+      <Text>{item.task}</Text>
+    </View>
+    
+  )
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>To-do List</Text>
+      <TextInput 
+        style={styles.input} 
+        placeholder="Add a todo..."
+        onChangeText={task => setTask(task)}
+        value={task}></TextInput>
+      <TouchableOpacity
+        style={styles.taskBotton}
+        >
+        <Text style={{color: "#fff", fontWeight: 700}}>Add New Task</Text>
+      </TouchableOpacity>
+      <FlatList
+        data={arr}
+        renderItem={renderItem}
+        keyExtractor={item => item.taskNum}
+      />
     </View>
   );
 }
@@ -17,5 +45,34 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+  title: {
+    margin: 25,
+    color: "#3d3b71",
+    fontSize: 50,
+    fontWeight: "700",
+  },
+  input: {
+    height: 40,
+    width: 300,
+    margin: 10,
+    padding: 10,
+    borderColor: "#3d3b71",
+    borderWidth: 1,
+    
+  },
+  taskBotton: {
+    backgroundColor: "#3d3b71",
+    margin: 10,
+    padding: 15,
+    borderRadius: 5,
+  },
+  item: {
+    height: 30, 
+    padding: 10,
+    margin: 5,
+    width: 300,
+    backgroundColor:"#ececec",
+  }
 });
